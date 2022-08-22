50541bb73357ce7fc1f133692738033e7feea5ca
DarK0707 <sezerpro7835@gmail.com>
2022-08-22 22:33:44 +0300

    deneme..

---
+++ variables.py
@@ -0,0 +1,12 @@
+# Variables are just nicknames
+# they're plain, lowercase words
+# examples : x , y , banana, phone_a_quail
+
+name = "Mathan Griffel"
+orphan_fee = 200
+teddy_bear_fee = 121.00
+
+total = orphan_fee + teddy_bear_fee
+
+print(name,"the total will be", total)
+
